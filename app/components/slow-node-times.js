faed32353e5fef9c50fdcf990408a415822412d6
Robert Jackson <rjackson@linkedin.com>
2017-02-04 18:01:33 -0500

    Use heimdalljs-graph to iterate nodes "smarter".

--- app/components/slow-node-times.js
+++ app/components/slow-node-times.js
@@ -1,56 +1,87 @@
 import Ember from 'ember';
+import heimdallGraph from 'heimdalljs-graph';
 
 const {
   computed
 } = Ember;
 
+function selfTime(node) {
+  for (let [statName, value] of node.statsIterator()) {
+    if (statName === 'time.self') {
+      return value;
+    }
+  }
+}
+
+function nodeTime(node) {
+  let nodeTotal = selfTime(node);
+
+  for (let childNode of node.dfsIterator((n) => n.label.broccoliNode)) {
+    nodeTotal += selfTime(childNode);
+  }
+
+  return nodeTotal;
+}
+
 export default Ember.Component.extend({
   nodes: computed('data', 'filter', 'pluginNameFilter', 'groupByPluginName', function() {
     let data = this.get('data');
-    let groupByPluginName = this.get('groupByPluginName');
-    let pluginName = this.get('pluginNameFilter');
     if (!data) { return []; }
 
-    let nodes = data.nodes.filter((node) => {
-      if (!node.label.broccoliNode) { return false; }
-      if (pluginName && node.label.broccoliPluginName !== pluginName) { return false; }
+    let nodes = [];
+    let graph = heimdallGraph.loadFromJSON(data);
 
-      return true;
-    })
-
-    if (groupByPluginName) {
-      let pluginNameMap = nodes.reduce((memo, node) => {
-        let pluginName = node.label.broccoliPluginName;
-        memo[pluginName] = memo[pluginName] || { count: 0, time: 0 };
-        memo[pluginName].time += node.stats.time.self;
-        memo[pluginName].count++;
-        return memo;
-      }, {})
-
-      nodes = [];
-      for (let pluginName in pluginNameMap) {
+    for (let node of graph.dfsIterator()) {
+      if (node.label.broccoliNode) {
         nodes.push({
-          label: { name: pluginName, broccoliPluginName: pluginNameMap[pluginName].count },
-          stats: {
-            time: { self: pluginNameMap[pluginName].time}
-          }
-        });
+          label: node.label,
+          time: nodeTime(node)
+        })
       }
     }
 
-    let addonNodes = nodes
+    //let groupByPluginName = this.get('groupByPluginName');
+    //let pluginName = this.get('pluginNameFilter');
+    // nodes = data.nodes.filter((node) => {
+    //   if (!node.label.broccoliNode) { return false; }
+    //   if (pluginName && node.label.broccoliPluginName !== pluginName) { return false; }
+
+    //   return true;
+    // })
+
+    // if (groupByPluginName) {
+    //   let pluginNameMap = nodes.reduce((memo, node) => {
+    //     let pluginName = node.label.broccoliPluginName;
+    //     memo[pluginName] = memo[pluginName] || { count: 0, time: 0 };
+    //     memo[pluginName].time += node.stats.time.self;
+    //     memo[pluginName].count++;
+    //     return memo;
+    //   }, {})
+
+    //   nodes = [];
+    //   for (let pluginName in pluginNameMap) {
+    //     nodes.push({
+    //       label: { name: pluginName, broccoliPluginName: pluginNameMap[pluginName].count },
+    //       stats: {
+    //         time: { self: pluginNameMap[pluginName].time}
+    //       }
+    //     });
+    //   }
+    // }
+
+    let sortedNodes = nodes
         .sort((a, b) => {
-          return b.stats.time.self - a.stats.time.self;
+          return b.time - a.time;
         });
 
-    return addonNodes;
+    return sortedNodes;
   }),
 
   totalTime: computed('nodes', function() {
     let nodes = this.get('nodes');
 
     return nodes.reduce(function(previousValue, node){
-      return previousValue + node.stats.time.self;
+      return previousValue + node.time;
     }, 0);
   }),
 
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,9 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
+    trees: {
+      vendor: 'node_modules/heimdalljs-graph'
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
@@ -19,6 +22,8 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+
+  app.import('vendor/dist/amd/heimdalljs-graph.js');
 
   return app.toTree();
 };
