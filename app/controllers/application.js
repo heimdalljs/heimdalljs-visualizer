0d6fb1c8d752517cb104ed51a3fa91e3a407e887
Robert Jackson <rjackson@linkedin.com>
2017-02-06 23:42:10 -0800

    Move shared upload functionality into simple modal.

---
+++ app/controllers/application.js
@@ -0,0 +1,34 @@
+import Ember from 'ember';
+import fetch from "ember-network/fetch";
+
+const {
+  inject
+} = Ember;
+
+export default Ember.Controller.extend({
+  graph: inject.service(),
+
+  actions: {
+    parseFile(event) {
+      let reader = new FileReader();
+      reader.onload = (e) => {
+        var contents = e.target.result;
+        this.get('graph').setGraph(JSON.parse(contents));
+        this.set('showUploadModal', false);
+      };
+
+      reader.readAsText(event.target.files[0]);
+    },
+
+    useSample(url) {
+      fetch(url)
+        .then((response) => {
+          return response.json();
+        })
+        .then((contents) => {
+          this.get('graph').setGraph(contents);
+          this.set('showUploadModal', false);
+        });
+    }
+  }
+})
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,33 +1,9 @@
 import Ember from 'ember';
-import fetch from "ember-network/fetch";
-import config from '../config/environment';
 
 const {
   inject
 } = Ember;
 
 export default Ember.Controller.extend({
-  graph: inject.service(),
-
-  actions: {
-    parseFile(event) {
-      let reader = new FileReader();
-      reader.onload = (e) => {
-        var contents = e.target.result;
-        this.get('graph').setGraph(JSON.parse(contents));
-      };
-
-      reader.readAsText(event.target.files[0]);
-    },
-
-    useSample(url) {
-      fetch(url)
-        .then((response) => {
-          return response.json();
-        })
-        .then((contents) => {
-          this.get('graph').setGraph(contents);
-        });
-    }
-  }
+  graph: inject.service()
 });
