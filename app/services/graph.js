8fa9418086d50473dd0ad71ef209cac2eba44aaf
Robert Jackson <rjackson@linkedin.com>
2017-02-08 07:02:46 -0800

    Use sessionStorage to make development simpler...

--- app/services/graph.js
+++ app/services/graph.js
@@ -1,13 +1,27 @@
 import Ember from 'ember';
+import config from '../config/environment';
 import heimdallGraph from 'heimdalljs-graph';
 
 const {
   getOwner
 } = Ember;
 
+const STORAGE_KEY = `${config.storageVersion}_graph-data`;
+
 export default Ember.Service.extend({
+  init() {
+    this._super(...arguments);
+
+    let data = sessionStorage.getItem(STORAGE_KEY);
+    if (data) {
+      this.setGraph(JSON.parse(data));
+    }
+  },
+
   setGraph(data) {
     let graph = heimdallGraph.loadFromJSON(data);
+
+    sessionStorage.setItem(STORAGE_KEY, JSON.stringify(data));
 
     this.set('data', data);
     this.set('graph', graph);
--- config/environment.js
+++ config/environment.js
@@ -20,7 +20,11 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
-    }
+    },
+
+    // bump this when making incompatible changes in
+    // localstorage / sessionStorage data formats
+    storageVersion: 2
   };
 
   if (environment === 'development') {
