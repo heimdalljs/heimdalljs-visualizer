59e962aa54d58d9a289ec338d162ef4e21f34655
Trent Willis <twillis@linkedin.com>
2017-02-08 14:44:27 -0800

    Fix failing linting/tests

--- tests/integration/components/basic-tree-test.js
+++ tests/integration/components/basic-tree-test.js
@@ -21,5 +21,5 @@ test('it renders', function(assert) {
     {{/basic-tree}}
   `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$().text().trim(), '');
 });
